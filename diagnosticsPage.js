b183a7e1c07010d958c309b6cd98f684fcc2287e
lucialup <40401039+lucialup@users.noreply.github.com>
2023-06-23 17:00:40 +0300

    Update html table generation

--- diagnosticsPage.js
+++ diagnosticsPage.js
@@ -2,24 +2,27 @@ const gi = require('node-gtk');
 const Gtk = gi.require('Gtk', '3.0');
 const execSync = require('child_process').execSync;
 const fs = require('fs');
-const { generateHTML } = require('./generateReport');
+const readline = require('readline');
 
 function parseLogs(logsString) {
     const lines = logsString.split('\n');
-    const logPattern = /\[(\s*\d+\.\d+)\]\s+Syscall:\s+(\w+),\s+File descriptor:\s+(\d+)(?:,\s+Path:\s+(.*?))?(?:,\s+Count:\s+(\d+))?(?:,\s+Buf:\s+(.*?))?,\s+Process ID:\s+(\d+)/;
+    const logPattern = /\[\s*(\d+\.\d+)\] Syscall: (\w+), FD: (-?\d+)(?:,\s+Filename: (.*?))?(?:,\s+Path: (.*?))?(?:,\s+Count: (\d+))?(?:,\s+Buf: (.*?))?(?:,\s+Flags: (\d+))?, PID: (\d+)/;
 
     const parsedLogs = [];
     for (let line of lines) {
         const match = line.match(logPattern);
         if (match) {
+            const [, timestamp, syscall, fd, filename, path, count, buf, flags, pid] = match;
             const log = {
-                timestamp: match[1].trim(),
-                syscall: match[2].trim(),
-                fd: match[3].trim(),
-                path: match[4] ? match[4].trim() : undefined,
-                count: match[5] ? match[5].trim() : undefined,
-                buf: match[6] ? match[6].trim() : undefined,
-                pid: match[7].trim(),
+                timestamp: parseFloat(timestamp),
+                syscall,
+                fd: parseInt(fd),
+                filename: filename || '-',
+                path: path || '-',
+                count: count ? parseInt(count) : '-',
+                buf: buf || '-',
+                flags: flags ? parseInt(flags) : '-',
+                pid: parseInt(pid),
             };
             parsedLogs.push(log);
         }
@@ -27,6 +30,70 @@ function parseLogs(logsString) {
     return parsedLogs;
 }
 
+
+function generateHTMLTable(logs) {
+    let html = `
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <title>Log Report</title>
+        <style>
+          table {
+            width: 100%;
+            border-collapse: collapse;
+          }
+          th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+          }
+          th {
+            background-color: #4CAF50;
+            color: white;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Log Report</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Timestamp</th>
+              <th>Syscall</th>
+              <th>FD</th>
+              <th>PID</th>
+              <th>Filename</th>
+              <th>Path</th>
+              <th>Flags</th>
+              <th>Count</th>
+              <th>Buf</th>
+            </tr>
+          </thead>
+          <tbody>`;
+
+    for (const log of logs) {
+        html += `
+      <tr>
+        <td>${log.timestamp}</td>
+        <td>${log.syscall}</td>
+        <td>${log.fd}</td>
+        <td>${log.pid}</td>
+        <td>${log.filename}</td>
+        <td>${log.path || ''}</td>
+        <td>${log.flags}</td>
+        <td>${log.count}</td>
+        <td>${log.buf || ''}</td>
+      </tr>`;
+    }
+
+    html += `
+          </tbody>
+        </table>
+      </body>
+    </html>`;
+
+    return html;
+}
 
 function createDiagnosticsPage(win) {
     let apkPath = '';
@@ -47,11 +114,12 @@ function createDiagnosticsPage(win) {
     fetchLogs.on('clicked', () => {
         execSync('adb shell dmesg > logs.txt');
 
-        const logs = parseLogs(fs.readFileSync('logs.txt', 'utf-8'));
-        const html = generateHTML(logs);
+        const logsString = fs.readFileSync('logs.txt', 'utf8');
+        const logs = parseLogs(logsString);
+        const html = generateHTMLTable(logs);
 
         fs.writeFileSync('report.html', html);
-        // execSync('open report.html'); // Open the report file
+        execSync('open report.html'); // Open the report file
     });
 
     const scanLogs = Gtk.Button.new();
